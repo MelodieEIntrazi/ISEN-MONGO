f7dbca90de30fcc38ee02369d2057b3a5c7d2736
marine <marine@localhost.localdomain>
2020-10-05 16:12:01 +0200

    Récupération des données de l'api + insertion des données dans une collection de la bdd locale

--- get_service.py
+++ get_service.py
@@ -1,2 +1,24 @@
-print("hello")
-print("coucou")
+import requests
+import json
+from pymongo import MongoClient
+from pprint import pprint
+
+
+client = MongoClient('localhost', 27017)
+db = client.test_velo
+lille = db.lille
+
+def get_vLille():
+    url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=10&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion&refine.commune=LILLE"
+    reponse = requests.request("GET", url)
+    reponse_json = json.loads(reponse.text.encode('utf8'))
+    lille.insert_one(reponse_json)
+    cursor = lille.find({})
+    for document in cursor: 
+        pprint(document)
+
+get_vLille()
+
+#mongodb+srv://admin:<password>@cluster0.5j4lv.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority
+#lien pour se co à la bdd
+#id  = admin mdp = root
