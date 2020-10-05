915ceffd75d9b98c2c8f40443711cdf3a8d5511b
marine <marine@localhost.localdomain>
2020-10-05 16:40:52 +0200

    Connection à MongoDB atlas

--- get_service.py
+++ get_service.py
@@ -3,22 +3,24 @@ import json
 from pymongo import MongoClient
 from pprint import pprint
 
-
-client = MongoClient('localhost', 27017)
-db = client.test_velo
-lille = db.lille
-
 def get_vLille():
+
+    #On tente de se connecter à la base de donnée
+    try:
+        client = MongoClient("mongodb+srv://admin:root@cluster0.5j4lv.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority")
+        print("Connection réussie")
+    except:
+        print("Impossible de se connecter")
+    db = client.info_velo
+    lille = db.lille
+
+    #On récupère les infos de l'api
     url = "https://opendata.lillemetropole.fr/api/records/1.0/search/?dataset=vlille-realtime&q=&rows=10&facet=libelle&facet=nom&facet=commune&facet=etat&facet=type&facet=etatconnexion&refine.commune=LILLE"
     reponse = requests.request("GET", url)
     reponse_json = json.loads(reponse.text.encode('utf8'))
     lille.insert_one(reponse_json)
     cursor = lille.find({})
-    for document in cursor: 
-        pprint(document)
+    #for document in cursor: 
+    #    pprint(document)
 
 get_vLille()
-
-#mongodb+srv://admin:<password>@cluster0.5j4lv.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority
-#lien pour se co à la bdd
-#id  = admin mdp = root
