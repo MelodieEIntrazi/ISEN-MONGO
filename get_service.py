fc20d10b940cc6aeac6c4a7e1a9f7329954f71b3
eintrazi <melodie.eintrazi@isen.yncrea.fr>
2020-10-09 15:35:24 +0200

    ques1

--- get_service.py
+++ get_service.py
@@ -25,4 +25,54 @@ def get_vLille():
     #for document in cursor: 
     #    pprint(document)
 
+def get_vLyon():
+    try:
+        client = MongoClient("mongodb+srv://dbUser:root@cluster0.5j4lv.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority")
+        print("Connection réussie")
+    except:
+        print("Impossible de se connecter")
+    db = client.info_velo
+    lyon = db.lyon
+
+    url = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=station-velov-grand-lyon&q=&facet=name&facet=commune&facet=bonus&facet=status&facet=available&facet=availabl_1&facet=availabili&facet=availabi_1&facet=last_upd_1"
+    reponse = requests.request("GET", url)
+    reponse_json = json.loads(reponse.text.encode('utf8'))
+    lyon.insert_one(reponse_json)
+    cursor = lyon.find({})
+
+def get_vParis():
+    try:
+        client = MongoClient("mongodb+srv://dbUser:root@cluster0.5j4lv.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority")
+        print("Connection réussie")
+    except:
+        print("Impossible de se connecter")
+    db = client.info_velo
+    paris = db.paris
+
+    url = "https://opendata.paris.fr/api/records/1.0/search/?dataset=velib-disponibilite-en-temps-reel&q=&facet=name&facet=is_installed&facet=is_renting&facet=is_returning&facet=nom_arrondissement_communes"
+    reponse = requests.request("GET", url)
+    reponse_json = json.loads(reponse.text.encode('utf8'))
+    paris.insert_one(reponse_json)
+    cursor = paris.find({})
+
+def get_vRennes():
+    try:
+        client = MongoClient("mongodb+srv://dbUser:root@cluster0.5j4lv.gcp.mongodb.net/<dbname>?retryWrites=true&w=majority")
+        print("Connection réussie")
+    except:
+        print("Impossible de se connecter")
+    db = client.info_velo
+    rennes = db.rennes
+
+    url = "https://data.rennesmetropole.fr/api/records/1.0/search/?dataset=etat-des-stations-le-velo-star-en-temps-reel&q=&facet=nom&facet=etat&facet=nombreemplacementsactuels&facet=nombreemplacementsdisponibles&facet=nombrevelosdisponibles"
+    reponse = requests.request("GET", url)
+    reponse_json = json.loads(reponse.text.encode('utf8'))
+    rennes.insert_one(reponse_json)
+    cursor = rennes.find({})
+
 get_vLille()
+get_vLyon()
+get_vParis()
+get_vRennes()
+
+
