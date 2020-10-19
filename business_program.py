70bd07d0dfd1ca40ef54ca246bfa3b1b6b7a4037
marine <marine@debian.home>
2020-10-19 12:50:19 +0200

    Touver la station par nom

--- business_program.py
+++ business_program.py
@@ -1,6 +1,7 @@
 import requests
 import json
 from pymongo import *
+import pymongo
 from pprint import pprint
 
 try:
@@ -13,17 +14,31 @@ db = client.info_velo
 
 def get_station_by_name(ville):
 
-    name = input("Entrer le nom de la station recherchée : ")
-    print(name)
+    name_station = input("Entrer le nom de la station recherchée : ")
 
     #On créé un dictionnaire des collections de ville : 
     listOfCollection = {"lille" : db.lille, "lyon" : db.lyon, "rennes" : db.rennes, "paris" :  db.paris}
     #On récupère la bonne collection
     collection_ville = listOfCollection[ville]
 
-    list_stations = collection_ville.find({"name": {"$regex" : name}})
-    for i in list_stations : 
-        print(i)
+    list_stations = collection_ville.find({"name": {"$regex" : name_station}}).sort("timestamp", -1)
+    list_stations_name = list_stations.distinct("name")
+    station = list_stations[0]
+
+    if(len(list_stations_name) > 1) :
+        print("Les stations suivantes correspondent à la recherche :")
+        i = 0
+        for station_name in list_stations_name : 
+            print(i , ' ) ' , station_name)
+            i += 1
+        number_station = int(input("Choissisez le numéro de la station voulue : " ))
+        name_station = list_stations_name[number_station]
+        station = collection_ville.find({"name": {"$regex" : name_station}}).sort("timestamp", -1)[0]
+    
+    print(station)
+
+        
+    
 
 def delete_station(ville, name):
 
